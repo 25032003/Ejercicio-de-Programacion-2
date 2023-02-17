da43aa04c48638c96e3ff73dcf1c7814d5a8172d
Brandón <Brandn@LAPTOP-K2SR54QM>
2023-02-17 15:25:04 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio de Programacion 2/Program.cs
@@ -0,0 +1,13 @@
+﻿byte numero1, numero2;
+ushort resultado;
+
+Console.WriteLine("Ingresa dos Numeros de dos cifras para Multiplicar:");
+Console.WriteLine("Numero1:");
+numero1 = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("Numero2:");
+numero2 = Convert.ToByte(Console.ReadLine());
+resultado = Convert.ToUInt16(numero1 * numero2);
+Console.WriteLine("El Resultado es:" + resultado);
+
+
+
